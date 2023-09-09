a06904dab4344123c091c787ad562c135372ce45
Gustavo Maia <Gustavomaian80@gmail.com>
2023-09-09 12:22:45 -0300

    Calculador de MC

---
+++ Exercicio.py
@@ -0,0 +1,13 @@
+
+nome = input ("Digite o seu nome: ")
+idade = int(input("Digite sua idade: "))
+peso = float(input("Digite o seu peso: "))
+altura = float(input("Digite sua altura "))
+               
+imc = peso / (altura * altura)
+
+print(f"Olá {nome};")
+print(f"Sua idade é de {idade} anos;")
+print(f"Seu imc é de {imc};")
+
+
