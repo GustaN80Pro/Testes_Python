1963f9d9c1d00162379cb79e7d628f8536c78710
Gustavo Maia <Gustavomaian80@gmail.com>
2023-09-16 10:14:13 -0300

    Calculadora Simples

--- calculadora_simples.py
+++ calculadora_simples.py
@@ -1,3 +1,24 @@
-numero =float(input("insira um numero")
-              
-operação = input("insira as operações para calcular os numeros (+ - * /):)")
+numero_um = float(input("insira um numero"))
+numero_dois = float(input("insira um numero"))
+operacao = input("insira as operações para calcular os numeros (+ - * /): ")
+
+resultado = ''
+if operacao =='+':
+    resultado = numero_um + numero_dois
+
+elif operacao == '-':
+    resultado = numero_um - numero_dois
+
+elif operacao == '*':
+    resultado = numero_um * numero_dois
+
+elif operacao == '/':
+    if numero_dois ==0:
+        print("Não é possivel dividir por zero")
+    else:
+        resultado = numero_um / numero_dois
+else:
+    print("Operação não reconhecida.")
+
+if resultado != '':
+        print(f"O resultado é: {resultado}")
